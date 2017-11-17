b128b80eeb6b6fa95732d7831db7c6e4858b95e5
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-11-16 23:38:41 -0600

    Added Buttons component.

---
+++ app/components/Buttons.js
@@ -0,0 +1,41 @@
+var React = require('react');
+
+class Buttons extends React.Component{
+  render(){
+    return(
+      <div>
+        <div>
+          <button>C</button>
+        </div>
+        <div>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>+</button>
+        </div>
+        <div>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>/</button>
+        </div>
+        <div>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+          <button>*</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = {
+  Buttons: Buttons
+}
--- app/components/Calculator.js
+++ app/components/Calculator.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var {Screen} = require('./Screen');
+var {Buttons} = require('./Buttons');
 
 class Calculator extends React.Component{
   render() {
     return(
       <div className="calc">
         <Screen />
+        <Buttons />
       </div>
     );
   }
