87e63e2b19bf270a26327a5137883f199aa30075
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-11-23 11:01:31 -0600

    Added postinstall script.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var path = require('path');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static('public'));
-// app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('build'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'public/views/index.html'));
+  res.sendFile(path.join(__dirname,'build/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
